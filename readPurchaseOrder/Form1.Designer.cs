dbc20ff2f0e01b42845a36f5de690ccef82a569d
dtziugomez <dtziugomez@gmail.com>
2020-11-29 19:38:54 -0600

    Agregar archivos de proyecto.

---
+++ readPurchaseOrder/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace readPurchaseOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lstText = new System.Windows.Forms.ListBox();
+            this.btnExportExcel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lstText
+            // 
+            this.lstText.FormattingEnabled = true;
+            this.lstText.HorizontalScrollbar = true;
+            this.lstText.Location = new System.Drawing.Point(13, 67);
+            this.lstText.Name = "lstText";
+            this.lstText.ScrollAlwaysVisible = true;
+            this.lstText.Size = new System.Drawing.Size(762, 173);
+            this.lstText.TabIndex = 1;
+            // 
+            // btnExportExcel
+            // 
+            this.btnExportExcel.Location = new System.Drawing.Point(119, 11);
+            this.btnExportExcel.Name = "btnExportExcel";
+            this.btnExportExcel.Size = new System.Drawing.Size(99, 23);
+            this.btnExportExcel.TabIndex = 2;
+            this.btnExportExcel.Text = "Export to Excel";
+            this.btnExportExcel.UseVisualStyleBackColor = true;
+            this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 261);
+            this.Controls.Add(this.btnExportExcel);
+            this.Controls.Add(this.lstText);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Read PurchaseOrder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListBox lstText;
+        private System.Windows.Forms.Button btnExportExcel;
+    }
+}
+
---
+++ readPurchaseOrder/Form1.cs
@@ -0,0 +1,253 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace readPurchaseOrder
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog dlg = new OpenFileDialog();
+        string conStrin = "Data Source=DELL\\SQLSERVER;Initial Catalog=Employees;User ID=sa;Password=Sql2008";
+        SqlConnection sqlCon;
+        SqlCommand sqlCmd;
+        string EmployeeId = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            sqlCon = new SqlConnection(conStrin);
+            sqlCon.Open();
+        }
+
+        #region pdf 
+
+        public static List<string> pdfText(string path)
+        {
+            List<string> Listtext = new List<string>();
+            try
+            {
+
+            PdfReader reader = new PdfReader(path);
+            string text = string.Empty;
+            for (int page = 1; page <= reader.NumberOfPages; page++)
+            {
+                text = PdfTextExtractor.GetTextFromPage(reader, page);
+                
+                foreach (var item in parseString(text))
+                {
+
+                    Listtext.Add(item);
+                }
+            }
+            reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+            return Listtext;
+        }
+        private static string[] parseString(string text)
+        {
+            char[] separator = { ' ' };
+            string[] items;
+            try
+            {
+            items=text.Split(separator);
+
+            }
+            catch (Exception ex)
+            {
+
+                items = new string[] { "" };
+            }
+            return items;
+        }
+        private static string GetTextFromPDF(string path)
+        {
+            StringBuilder text = new StringBuilder();
+            using (PdfReader reader = new PdfReader(path))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+            }
+
+            return text.ToString();
+        }
+        #endregion
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            
+            // set file filter of dialog   
+            dlg.Filter = "pdf files (*.pdf) |*.pdf;";
+            try
+            {
+            dlg.ShowDialog();
+            if (dlg.FileName != null)
+            {
+                this.lstText.Items.Clear();
+                foreach (var item in pdfText(dlg.FileName))
+                {
+
+                 this.lstText.Items.Add(item);
+                }
+            }
+
+            }
+            catch (Exception ex)
+            {
+
+             
+            }
+        }
+        #region exports
+
+        private void ExportPDFToExcel()
+        {
+            StringBuilder text = new StringBuilder();
+            if (dlg.FileName != null)
+            {
+                PdfReader pdfReader = new PdfReader(dlg.FileName);
+            for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+            {
+                ITextExtractionStrategy strategy = new LocationTextExtractionStrategy();
+                string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
+                currentText = Encoding.UTF8.GetString(Encoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.UTF8.GetBytes(currentText)));
+                text.Append(currentText);
+                pdfReader.Close();
+            }
+            //Response.Clear();
+            //Response.Buffer = true;
+            //Response.AddHeader("content-disposition", "attachment;filename=ReceiptExport.xls");
+            //Response.Charset = "";
+            //Response.ContentType = "application/vnd.ms-excel";
+            //Response.Write(text);
+            //Response.Flush();
+            //Response.End();
+            }
+        }
+
+        
+        #endregion
+        private void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            ExportPDFToExcel();
+        }
+
+        #region base datos
+        private void SaveData()
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                DataTable dtData = new DataTable();
+                sqlCmd = new SqlCommand("spEmployee", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@ActionType", "SaveData");
+                sqlCmd.Parameters.AddWithValue("@EmployeeId", EmployeeId);
+                //sqlCmd.Parameters.AddWithValue("@Name", textBox1.Text);
+                //sqlCmd.Parameters.AddWithValue("@City", textBox2.Text);
+                //sqlCmd.Parameters.AddWithValue("@Department", textBox4.Text);
+                //sqlCmd.Parameters.AddWithValue("@Gender", comboBox1.Text);
+                int numRes = sqlCmd.ExecuteNonQuery();
+                if (numRes > 0)
+                {
+                    MessageBox.Show("Data saved successfully !!!");
+
+                }
+                else MessageBox.Show("Please try again");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error:-" + ex.Message);
+            }
+        }
+        private DataTable FetchEmpDetails()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            DataTable dtData = new DataTable();
+            sqlCmd = new SqlCommand("spEmployee", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ActionType", "FetchData");
+            SqlDataAdapter sqlSda = new SqlDataAdapter(sqlCmd);
+            sqlSda.Fill(dtData);
+            return dtData;
+        }
+        private DataTable FetchEmpRecords(string empId)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            DataTable dtData = new DataTable();
+            sqlCmd = new SqlCommand("spEmployee", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ActionType", "FetchRecord");
+            sqlCmd.Parameters.AddWithValue("EmployeeId", empId);
+            SqlDataAdapter sqlSda = new SqlDataAdapter(sqlCmd);
+            sqlSda.Fill(dtData);
+            return dtData;
+        }
+        private void Delete()
+        {
+            if (!string.IsNullOrEmpty(EmployeeId))
+            {
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                    {
+                        sqlCon.Open();
+                    }
+                    DataTable dtData = new DataTable();
+                    sqlCmd = new SqlCommand("spEmployee", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@ActionType", "DeleteData");
+                    sqlCmd.Parameters.AddWithValue("@EmployeeId", EmployeeId);
+                    int numRes = sqlCmd.ExecuteNonQuery();
+                    if (numRes > 0)
+                    {
+                        MessageBox.Show("Record deleted successfully!!!");
+                        
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please try Again!!!");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Error:-" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select a record");
+            }
+        }
+
+        #endregion
+    }
+}
